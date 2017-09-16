7b940a36e70fcf980a9ad3b2c52b69c97eab558f
Inho Choi <hasbegun@gmail.com>
2017-09-15 17:21:18 -0700

    Add bin dir (build) just incase sharing.

---
+++ build/moc_IrisCompare.cpp
@@ -0,0 +1,164 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'IrisCompare.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../IrisCompare.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'IrisCompare.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_IrisCompare_t {
+    QByteArrayData data[14];
+    char stringdata0[165];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_IrisCompare_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_IrisCompare_t qt_meta_stringdata_IrisCompare = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "IrisCompare"
+QT_MOC_LITERAL(1, 12, 11), // "OpenAVIFile"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 15), // "OpenIrisAVIFile"
+QT_MOC_LITERAL(4, 41, 7), // "OpenCAM"
+QT_MOC_LITERAL(5, 49, 13), // "SelectCascade"
+QT_MOC_LITERAL(6, 63, 12), // "checkQuality"
+QT_MOC_LITERAL(7, 76, 10), // "StartMatch"
+QT_MOC_LITERAL(8, 87, 12), // "openLeftEye1"
+QT_MOC_LITERAL(9, 100, 12), // "openLeftEye2"
+QT_MOC_LITERAL(10, 113, 13), // "openRightEye1"
+QT_MOC_LITERAL(11, 127, 13), // "openRightEye2"
+QT_MOC_LITERAL(12, 141, 7), // "goMatch"
+QT_MOC_LITERAL(13, 149, 15) // "generateReports"
+
+    },
+    "IrisCompare\0OpenAVIFile\0\0OpenIrisAVIFile\0"
+    "OpenCAM\0SelectCascade\0checkQuality\0"
+    "StartMatch\0openLeftEye1\0openLeftEye2\0"
+    "openRightEye1\0openRightEye2\0goMatch\0"
+    "generateReports"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_IrisCompare[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      12,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   74,    2, 0x0a /* Public */,
+       3,    0,   75,    2, 0x0a /* Public */,
+       4,    0,   76,    2, 0x0a /* Public */,
+       5,    0,   77,    2, 0x0a /* Public */,
+       6,    0,   78,    2, 0x0a /* Public */,
+       7,    0,   79,    2, 0x0a /* Public */,
+       8,    0,   80,    2, 0x0a /* Public */,
+       9,    0,   81,    2, 0x0a /* Public */,
+      10,    0,   82,    2, 0x0a /* Public */,
+      11,    0,   83,    2, 0x0a /* Public */,
+      12,    0,   84,    2, 0x0a /* Public */,
+      13,    0,   85,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void IrisCompare::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        IrisCompare *_t = static_cast<IrisCompare *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->OpenAVIFile(); break;
+        case 1: _t->OpenIrisAVIFile(); break;
+        case 2: _t->OpenCAM(); break;
+        case 3: _t->SelectCascade(); break;
+        case 4: _t->checkQuality(); break;
+        case 5: _t->StartMatch(); break;
+        case 6: _t->openLeftEye1(); break;
+        case 7: _t->openLeftEye2(); break;
+        case 8: _t->openRightEye1(); break;
+        case 9: _t->openRightEye2(); break;
+        case 10: _t->goMatch(); break;
+        case 11: _t->generateReports(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject IrisCompare::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_IrisCompare.data,
+      qt_meta_data_IrisCompare,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *IrisCompare::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *IrisCompare::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_IrisCompare.stringdata0))
+        return static_cast<void*>(const_cast< IrisCompare*>(this));
+    if (!strcmp(_clname, "Ui_IrisCompare"))
+        return static_cast< Ui_IrisCompare*>(const_cast< IrisCompare*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int IrisCompare::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 12)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 12;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 12)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 12;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
