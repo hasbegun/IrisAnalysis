14ec9f6244cb02dba8aeda828a207879dac014e6
Inho Choi <hasbegun@gmail.com>
2017-10-11 13:50:56 -0700

    merge Utility.cpp

--- IrisCompare.cpp
+++ IrisCompare.cpp
@@ -670,7 +670,65 @@ char *IrisCompare::openFileName(const char *path, const char *title, const char 
 
 }
 
+//Widget: Clean up before new image widget starts
 void IrisCompare::clearWidget()
 {
+    leftFileName1 = NULL;
+    rightFileName1 = NULL;
+    leftFileName2 = NULL;
+    rightFileName2 = NULL;
 
+    imgLeftWidget1->reset();
+    imgLeftWidget2->reset();
+    imgRightWidget1->reset();
+    imgRightWidget2->reset();
+
+    txtLeftResult->setText("?");
+    txtLeftResult->setStyleSheet("QLabel {background-color: window}");
+    txtRightResult->setText("?");
+    txtRightResult->setStyleSheet("QLabel {background-color: window}");
+
+    txtLeftDataType1->setText("Data Type here");
+    txtLeftDataType2->setText("Data Type here");
+    txtRightDataType1->setText("Data Type here");
+    txtRightDataType2->setText("Data Type here");
+
+    this->repaint();
+}
+
+//Enter the output name in commend
+char* IrisCompare::getOutputFName(int i, string currentPath, string type)
+{
+    std::string str;
+    cout << i <<"Enter " << type <<": ";
+    std::getline(cin, str);
+    if(str=="") {
+        cout << "Please input the output name here." << endl;
+        cout << i <<"Enter " << type <<": ";
+        std::getline(cin, str);
+        if(str=="") {
+            cout << "ERROR: Failed to enter the input" << endl;
+            return NULL;
+        }
+    }
+
+    if(currentPath=="") {
+        currentPath = (string)QDir::currentPath().append("/../output/").toLatin1();
+    }
+    std::string fName = currentPath+str;
+    char* myFileName = new char[fName.size()+1];
+    strcpy(myFileName, fName.c_str());
+
+    return myFileName;
+}
+
+int IrisCompare::getString(std::string title)
+{
+    std::string str;
+    int val = 0;
+    cout << "Enter "<< title <<": ";
+    std::getline(cin, str);
+    val = atoi(str.c_str());
+
+    return val;
 }
